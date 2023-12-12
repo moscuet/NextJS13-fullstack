ed07f05384647b7e611db987325bef3d05ae0439
moscuet <mos.cuet@gmail.com>
2023-12-12 20:12:18 +0200

    update DocumentationTabs comp

--- src/components/DocumentationTabs.tsx
+++ src/components/DocumentationTabs.tsx
@@ -1,17 +1,37 @@
+'use client'
 import { FC } from 'react'
-import { Tabs, TabsList, TabsTrigger } from './ui/Tabs'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/Tabs'
+import CodeSnippet from './CodeSnippet'
+import { nodejs, php, python } from '@/const/code-snippet'
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 
 interface DocumentationTabsProps {
 
 }
 
 const DocumentationTabs: FC = () => {
-    return <Tabs className='max-w-2xl w-full'>
+    return <Tabs className='max-w-2xl w-full lg:text-left text-center'>
         <TabsList>
-            <TabsTrigger value={'React'}> React</TabsTrigger>
-            <TabsTrigger value={'TypeScript'}> TypeScript</TabsTrigger>
-            <TabsTrigger value={'Python'}> Python</TabsTrigger>
+            <TabsTrigger value={'nodejs'}> Node JS</TabsTrigger>
+            <TabsTrigger value={'php'}> Php</TabsTrigger>
+            <TabsTrigger value={'python'}> Python</TabsTrigger>
         </TabsList>
+        <TabsContent value={'nodejs'}>
+            <SimpleBar>
+            <CodeSnippet animated CodeSnippet={php} language={'nodejs'} show></CodeSnippet>
+            </SimpleBar>
+      </TabsContent>
+        <TabsContent value={'php'}>
+            <SimpleBar>
+            <CodeSnippet animated CodeSnippet={php} language={'php'} show></CodeSnippet>
+            </SimpleBar>
+      </TabsContent>
+        <TabsContent value={'python'}>
+            <SimpleBar>
+            <CodeSnippet animated CodeSnippet={php} language={'python'} show></CodeSnippet>
+            </SimpleBar>
+      </TabsContent>
     </Tabs> 
 }
 
